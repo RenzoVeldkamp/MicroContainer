3f83c5440b2caa03cc9714b950a8ad659fb88545
Renzo Veldkamp <renzo.veldkamp@centric.eu>
2017-09-01 18:01:08 +0200

    OSS-infra working

--- OSS/nodejsmovies/server.js
+++ OSS/nodejsmovies/server.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var path = require('path');
 var app = express();
 
 var movies = require('./movies');
 app.use('/api/movies', movies);
 
-app.listen(process.env.PORT || 4201, function () {
-    console.info('The server is listening at port ' + (process.env.PORT || 4201));
+app.listen(process.env.PORT || 80, function () {
+    console.info('The server is listening at port ' + (process.env.PORT || 80));
 });
