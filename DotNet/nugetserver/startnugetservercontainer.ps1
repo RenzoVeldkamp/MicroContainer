10f84ad2c567111c8c8d3248fa37b366dabf6496
Renzo Veldkamp <renzo.veldkamp@centric.eu>
2019-10-01 17:04:30 +0200

    feeble attempt for nuget in container.... (GRRRRR)

---
+++ DotNet/nugetserver/startnugetservercontainer.ps1
@@ -0,0 +1,6 @@
+﻿# run container:
+# docker run --detach=true --publish 5000:80 --env NUGET_API_KEY=centricms --env BASE_URL=/nuget --volume ${PWD}/nuget/database:/var/www/db --volume ${PWD}/nuget/packages:/var/www/packagefiles --name nuget-server sunside/simple-nuget-server
+
+# push package:
+# dotnet nuget push .\DierenHok.Communication\DierenHok.Communication.1.0.0.nupkg -s http://localhost:5000 --api-key centricms
+
